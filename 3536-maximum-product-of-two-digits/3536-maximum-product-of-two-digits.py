5724b1a958d04f77d5f7a00213efab2d7831d37c
ΛʙʜɪɴᴀV <sng23cs002@sngce.ac.in>
2026-08-02 19:52:57 +0530

    Time: 18 ms (5.04%), Space: 12.4 MB (57.98%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -4,8 +4,6 @@ class Solution(object):
         large=-1
         for i in range(0,len(arr)-1):
             for k in range(i+1,len(arr)):
-                if i==k:
-                    continue
                 no=int(arr[i])*int(arr[k])
                 if no > large:
                     large=no
